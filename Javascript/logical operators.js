0b663dac7df9ee07f428c7be3682e74c934b303b
Kiran-Sawant <kbsawant1005@gmail.com>
2021-01-31 00:32:06 +0530

    Operators

---
+++ Javascript/logical operators.js
@@ -0,0 +1,35 @@
+//_________Comparison operators____________//
+
+// Greater than Operator
+3 > 2;
+2 > 3;
+
+// Less than Operator
+2 < 13;
+
+// Greater than Equal to Operator
+10 >= 5;
+
+// Less than Equal to Operator
+12 <= 15;
+
+//__________Equality Operators____________//
+// Magnitude equality, checks if the magnitude is equal regardless of variable type
+
+2 == "2";
+2 == 2;
+
+2 != 5;
+
+// Magnitude and type equality & inequality
+2 === "2";
+2 !== "2";
+
+// behavior with NaN, undefined and NaN 
+null == undefined;  // true
+NaN == NaN;        // false (One non-number might not be equal to another non-number)
+
+// and & or operator
+
+1 == "1" && 2 == "2";           // and operator
+1 == "12" || 2 == "2";          // or operator
---
+++ Javascript/mathematical operators.js
@@ -0,0 +1,18 @@
+// addition
+// alert pops a messagebox on browser
+alert(5 + 5)
+
+// subtraction
+alert(5 - 5)
+
+// multiplication
+alert(5 * 5)
+
+// Division
+alert(5 / 5)
+
+// modulo, returns a remainder for rational quotiant, eg: 5%4 = 1
+alert(5 % 5)
+
+// power
+alert(5 ** 5)
