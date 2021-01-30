7b3c182784ca01fd900ae5c81da08d6dfd4c45cd
Kiran-Sawant <kbsawant1005@gmail.com>
2021-01-31 00:33:20 +0530

    Javascript & browser

---
+++ Javascript/Browser Operations.js
@@ -0,0 +1,8 @@
+// shows the message in an alert box.
+alert("Hello world")
+
+// Prints on Browser console
+console.log("Hey How are you")
+
+// pops a browser prompt that asks for user-input
+var k = prompt("Input Your age")
