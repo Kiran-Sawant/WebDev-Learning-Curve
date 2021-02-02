0b144345af5e8686adc51a2213a3146e8edb5e3c
Kiran-Sawant <kbsawant1005@gmail.com>
2021-02-03 00:31:33 +0530

    numerical methods

--- Javascript/mathematical operators.js
+++ Javascript/mathematical operators.js
@@ -16,3 +16,21 @@ alert(5 % 5)
 
 // power
 alert(5 ** 5)
+
+//___________numerical methods_____________//
+var x = 2.456789
+
+// .toFixed() returns a string, with the number written with a specified number of decimals
+x.toFixed(3)
+
+// toPrecision() returns a string, with a number written with a specified length:
+x.toPrecision(4)
+
+// here, y is a string
+var y = prompt("Enter a number: ")
+
+var k = Number(y)           // converts strings to number.
+
+var k = parseInt(y)         // searches for first occurence of int in a string & stores it as number.
+
+var k = parseFloat(y)       // searches for first occurence of float in a string & storres it as mnumber.
