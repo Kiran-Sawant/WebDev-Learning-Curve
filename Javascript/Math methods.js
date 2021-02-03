d1dbd32a017a1b95bf3e6c847829d13338526dc4
Kiran-Sawant <kbsawant1005@gmail.com>
2021-02-03 18:56:48 +0530

    Math methods in javaScript

---
+++ Javascript/Math methods.js
@@ -0,0 +1,25 @@
+Math.round(5.6789)          // rounds off the number.
+Math.fround(5.6789)         // returns float with nearest minimum pricision.
+Math.trunc(5.6789)          // drops digits after decimal & returns the number only.
+
+Math.floor(-3.56)           // returns lowest round integer, will return -4
+Math.ceil(-3.56)            // returns highset round integer, will return -3
+
+Math.pow(9, 3)              // returns first arg raised to second arg
+
+Math.sqrt(64)               // returns squrt of passed arg
+
+Math.cbrt(27)               // cube root of passed arg
+
+Math.abs(6.54545)           // returnes magnitude(without -/+)
+
+//____________Constants____________//
+
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E 
