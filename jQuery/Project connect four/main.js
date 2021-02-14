0e8cfefaf4e22cbe191a01cf715ee15b0117227b
Kiran-Sawant <kbsawant1005@gmail.com>
2021-02-14 18:19:42 +0530

    Added vertical Traverser

--- jQuery/Project connect four/main.js
+++ jQuery/Project connect four/main.js
@@ -1,5 +1,5 @@
 var playerOne = prompt("Player One enter your name, you will be Blue.")
-var playerTwo = prompt("Player One enter your name, you will be Red.")
+var playerTwo = prompt("Player Two enter your name, you will be Red.")
 
 
 var state = "blue";
@@ -81,6 +81,27 @@ function traverse(state){
         } else if(taData.eq(i)[0].className != state && taData.eq(i)[0].className != ""){
             counter = 0;
             continue;
+        }
+    }
+    // vertical traverser
+    for(i=0; i<7; i++){
+        for(j=i; j < (i + 36); j+=7){
+            // if no class.
+            if(taData.eq(j)[0].className == ''){
+                continue;
+            }
+            // if class == state
+            else if(taData.eq(j)[0].className == state) {
+                counter += 1;
+                if(counter == 4){
+                    alert(`${state} Won, refresh to restart`)
+                } else {continue;}
+            }
+            // if non-empty class not equal
+            else if(taData.eq(j)[0].className != state && taData.eq(j)[0].className != ""){
+                counter = 0;
+                continue;
+            }
         }
     }
     console.log(counter)
