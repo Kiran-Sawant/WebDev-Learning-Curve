a548dfa37fb16fea64a10879fae34faa357a8a7d
Kiran-Sawant <kbsawant1005@gmail.com>
2021-02-20 15:14:11 +0530

    created ModelForm for user registration

---
+++ Django/learning_users/basic_app/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+from basic_app import models
+
+# Base form.
+class UserForm(forms.ModelForm):
+
+    # modifying the predefined password field.
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileInfo(forms.ModelForm):
+
+    class Meta():
+
+        model = models.UserProfileInfo
+        fields = ('portfolio_site', 'profile_pic') 
