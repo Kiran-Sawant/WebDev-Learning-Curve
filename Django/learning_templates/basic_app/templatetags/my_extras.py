13dcc33f563ea95c0dd6f4cd5d91ef1dbc04f711
Kiran-Sawant <kbsawant1005@gmail.com>
2021-02-25 13:23:50 +0530

    adding custom template filters

---
+++ Django/learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,18 @@
+"""In this script we create a custom template filter for our template."""
+
+from django import template
+
+# Creating an instance of Library class.
+# It is used to register our custom created filters and template tags.
+register = template.Library()
+
+# registering using decorators.
+@register.filter(name='cut')
+def cut(value, arg):
+    """This cuts out all values of arg from the value string."""
+
+    return value.replace(arg, '')
+
+# Registering or custom filter.
+# first arg is the name by which we will call it in template, & second is the function itself
+#register.filter('cut', cut)
