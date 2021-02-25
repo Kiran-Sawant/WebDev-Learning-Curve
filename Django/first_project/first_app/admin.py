7991374211e34d0b904a09cb9738f2719bd60310
Kiran-Sawant <kbsawant1005@gmail.com>
2021-02-25 13:05:28 +0530

    app admin

---
+++ Django/first_project/first_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+# Importing Models
+from first_app import models
+
+# Register your models here.
+admin.site.register(models.AccessRecord)
+admin.site.register(models.Topic)
+admin.site.register(models.WebPage)
