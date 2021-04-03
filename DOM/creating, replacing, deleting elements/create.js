cf0cb78e7ad5196d039f954d0eb945d4d6867c05
Kiran-Sawant <kbsawant1005@gmail.com>
2021-04-04 00:48:47 +0530

    tidying up

--- DOM/creating, replacing, deleting elements/create.js
+++ DOM/creating, replacing, deleting elements/create.js
@@ -1,7 +1,7 @@
 
 function addItem() {
-    var item = document.createElement('li')
-    var itemText = document.createTextNode('Four')    
+    var item = document.createElement('li')             // Creats <li></li>
+    var itemText = document.createTextNode('Four')      // creates a textContent
 
     item.appendChild(itemText)
 
